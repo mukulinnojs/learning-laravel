71d0d6c241b97d3eaee7150b3842c1cc5a7d715c
Mukul Dogra <mukul.innojs@gmail.com>
2025-05-27 13:31:09 +0530

    Simple Mail Sidebar Added

--- simplemail/resources/js/app.js
+++ simplemail/resources/js/app.js
@@ -1,1 +1,22 @@
-import './bootstrap';
+document.addEventListener('DOMContentLoaded', () => {
+    const composebtn = document.querySelector('.js-compose-btn');
+    const closebtn = document.querySelector('.js-close-modal');
+    const overlay = document.querySelector('.js-compose-mail');
+    const modalBox = document.getElementById('compose-modal');
+
+    composebtn.addEventListener('click', () => {
+        toggleModal();
+    })
+    closebtn.addEventListener('click', () => {
+        toggleModal();
+    })
+    overlay.addEventListener('click', () => {
+        toggleModal();
+    })
+    modalBox.addEventListener('click', (e) => e.stopPropagation()); // 🛑 prevents toggle
+})
+
+function toggleModal() {
+    const modal = document.querySelector('.js-compose-mail');
+    modal.classList.toggle('active');
+}
